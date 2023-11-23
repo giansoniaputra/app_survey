5058b7fe76c11bc38d7671e5185b23c743bf4af7
giansoniaputra <giansonia555@gmail.com>
2023-11-23 15:23:12 +0700

    Reverensi ke wilayah id

--- resources/views/layouts/horizontal-bar.blade.php
+++ resources/views/layouts/horizontal-bar.blade.php
@@ -8,12 +8,6 @@
                         <a class="nav-link dropdown-toggle arrow-none" href="/dashboard" id="topnav-dashboards" role="button" aria-haspopup="true" aria-expanded="false">
                             <i class="ri-dashboard-3-line"></i>Dashboards
                         </a>
-                        <a class="nav-link dropdown-toggle arrow-none" href="/input1" id="topnav-dashboards" role="button" aria-haspopup="true" aria-expanded="false">
-                            <i class="ri-dashboard-3-line"></i>Input Data
-                        </a>
-                        <a class="nav-link dropdown-toggle arrow-none" href="/input2" id="topnav-dashboards" role="button" aria-haspopup="true" aria-expanded="false">
-                            <i class="ri-dashboard-3-line"></i>Input Data 2
-                        </a>
                     </li>
                 </ul>
             </div>
